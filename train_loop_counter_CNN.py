a56ce669c8d84c6892679df2b0ed0bdc7e251f03
calvinpash <cpp5317@psu.edu>
2021-03-10 00:23:48 -0500

    Added adjustments to make code more plug-n-play

--- train_loop_counter_CNN.py
+++ train_loop_counter_CNN.py
@@ -2,43 +2,57 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 import torchvision
 from torchvision import transforms, utils
-from data.dataset_definitions import LoopsDataset, ToTensor
+# from data.dataset_definitions import LoopsDataset, ToTensor
 from pandas import DataFrame, concat, read_csv
 import numpy as np
 from sys import argv, exit
+from PIL import Image
 import os
 #Create Dataset
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 8, padding = 4)#3 input channels, 12 output, 8x8 kernel
-        self.pool = nn.MaxPool2d(4, 4)#summarizes the most activated presence of a feature
-        self.conv2 = nn.Conv2d(6, 16, 8, padding = 4)
-        self.fc1 = nn.Linear(16 * 4 * 4, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 21)
+        padding1=3
+        filter1=7
+
+        self.conv1 = nn.Conv2d(3, 16, filter1, padding = padding1)#3 input channels, 12 output, 8x8 kernel
+        size1=64+2*padding1-(filter1-1)
+        #print(size1)
+        pool_size=8 #pooling kernel
+        pool_stride=8 #pooling size
+
+        self.pool = nn.MaxPool2d(pool_size, pool_stride)#summarizes the most activated presence of a feature
+
+        size2=np.floor((pool_size/2+size1)/pool_stride).astype(int)
+        #print(size2)
+
+        padding3=2
+        filter3=5
+
+        self.conv2 = nn.Conv2d(16, 8, filter3, padding = padding3)
+        size3=size2+2*padding3-(filter3-1)
+        #print(size3)
+
+        #self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        size4=np.floor((pool_size/2+size3)/pool_stride).astype(int)
+        #print(size4)
+        self.fc1 = nn.Linear(8 * size4 * size4, 120)
+        self.fc2 = nn.Linear(120, 88)
+        self.fc3 = nn.Linear(88,8)
 
     def forward(self, x):
         x = self.conv1(x)
-        # print(x.shape)#6x57x57->6x19x19
         x = self.pool(F.relu(x))
-        # print(x.shape)#6x57x57->6x19x19
         x = self.conv2(x)
-        # print(x.shape)#16x12x12->16x4x4
         x = self.pool(F.relu(x))
-        # print(x.shape)#16x12x12->16x4x4
         x = x.view(-1, self.num_flat_features(x))
-        # print(x.shape)
         x = F.relu(self.fc1(x))
-        # print(x.shape)
         x = F.relu(self.fc2(x))
-        # print(x.shape)
         x = self.fc3(x)
-        # print(x.shape)
         return x
 
 
@@ -49,6 +63,65 @@ class Net(nn.Module):
             num_features *= s
         return num_features
 
+class LoopsDataset(Dataset):
+
+    def __init__(self, csv_file, root_dir, transform=None, hot = False):
+        self.loops_frame = read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+        self.hot = hot
+
+    def __len__(self):
+        return len(self.loops_frame)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir, str(self.loops_frame.iloc[idx, 0]) + ".png")
+
+        tmp=Image.open(img_name).convert('RGB')
+
+        image = np.zeros([tmp.size[0],tmp.size[1],3])
+        image[:,:,0],image[:,:,1],image[:,:,2]=tmp.split()
+        #print(image.shape)
+        text = self.loops_frame.iloc[idx, 1].replace("%2B","+").replace("%23","#").replace("%25","%").replace("%26","&")
+
+        if self.hot: #If using one-hot encoded list (MSE, . . .)
+            loops = np.zeros(21)
+            loops[self.loops_frame.iloc[idx, 2]] = 1
+        else: #If using label index (CrossEntropy, . . .
+            loops = self.loops_frame.iloc[idx, 2]
+        sample = {'image': image, 'loops': loops, 'text': text}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+class ToTensor(object):
+    def __call__(self, sample):
+        image, loops, text = sample['image'], sample['loops'], sample['text']
+
+        hot = (type(loops) == np.ndarray)
+        if image.shape[2] == 4:#if the image has a color channel
+            image = image[:,:,:3]#get rid of alpha channel
+        elif len(image.shape) == 2:#if the image is grayscale, create color channels
+            image = np.array([np.array([np.array([px, px, px]) for px in r]) for r in image])
+        image = image/255.#Does all the normalization for me
+        #Convert image shape from (64,64,3) to (3,64,64)
+
+        #print(image.shape)
+        image = image.transpose((2, 0, 1))
+        if hot:
+            return {'image': torch.from_numpy(image).float(),
+                    'loops': torch.from_numpy(loops).float(),
+                    'text': text}
+        else:
+            return {'image': torch.from_numpy(image).float(),
+                    'loops': loops,
+                    'text': text}
+
 def main(args):
     e = 100
     b = 1000
@@ -58,6 +131,8 @@ def main(args):
     o_append = False
     hot = False
     target = "./loops_counter_net.pth"
+    data_file='data/test_dat.csv'
+    data_dir='data/test_images/'
     for arg in args:#Takes in command line arguments; less sugar but more rigorous
         try: arg = int(arg)
         except: pass
@@ -92,20 +167,24 @@ def main(args):
     # print(f"Append to model: %r" % append)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    vals=np.ones(8)
+    vals=vals/np.linalg.norm(vals)
+    weights=torch.FloatTensor(vals).to(device)
+
     print(f"Using device: %s" % device)
     net = Net()
     net.to(device)
     if hot:
-        criterion = nn.MSELoss()
+        criterion = nn.MSELoss(weight = weights)
     else:
-        criterion = nn.CrossEntropyLoss()
+        criterion = nn.CrossEntropyLoss(weight = weights)
     print("Loading Data")
     # device = torch.device('cuda' if torch.cuda)
-    loops_dataset = LoopsDataset(hot=hot, csv_file='data/dat.csv', root_dir='data/images/', transform = transforms.Compose([ToTensor()]))
+    loops_dataset = LoopsDataset(hot=hot, csv_file=data_file, root_dir=data_dir, transform = transforms.Compose([ToTensor()]))
     dataloader = DataLoader(loops_dataset, batch_size = b, shuffle = True, num_workers = nw)
 
-    if len(loops_dataset) != len(os.listdir("./data/images")):
-        print(f"Found %d entries and %d images. Killing script" % (len(loops_dataset), len(os.listdir("./data/images"))))
+    if len(loops_dataset) != len(os.listdir(data_dir)):
+        print(f"Found %d entries and %d images. Killing script" % (len(loops_dataset), len(os.listdir(data_dir))))
         exit()
 
     classes = tuple(range(21))
@@ -113,6 +192,7 @@ def main(args):
     #We use MSELoss here because our output is a vector of length 21
     optimizer = optim.SGD(net.parameters(), lr=0.1)#,momentum = 0.9)
     print("Loaded\n")
+
 
 
     if o:
@@ -145,7 +225,7 @@ def main(args):
         running_loss = 0.0
         for i, data in enumerate(dataloader, 0):
             inputs, loops, text = data['image'].to(device), data['loops'].to(device), data['text']
-            print(type(inputs), type(loops), type(text))
+            # print(type(inputs), type(loops), type(text))
             optimizer.zero_grad()
             outputs = net(inputs)
             loss = criterion(outputs, loops)
